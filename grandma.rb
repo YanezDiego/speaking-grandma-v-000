d6100a90a36111179772b4755aa7d59f4191bf06
Diego Yanez <diegosyanez92@gmail.com>
2018-03-26 21:32:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma (speak)
   if speak == speak.upcase
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif speak ==
+  elsif speak == 
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
